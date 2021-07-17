705abe3715058a2975bbd35733ed3d1b90b464a2
why <weihaoyuan2@126.com>
2021-07-17 15:12:21 +0800

    更新

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
@@ -29,4 +29,19 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    return HttpResponse("你在给第{}题投票".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': '选择错误',
+        })
+    else:
+        select_choice.votes += 1
+        select_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
